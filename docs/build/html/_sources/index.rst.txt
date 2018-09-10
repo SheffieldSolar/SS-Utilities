218a7538f9ea7bfa8ade6305cfdc5269d21e337a
SSFJT2\Jamie <jamie.taylor@sheffield.ac.uk>
2018-09-10 12:20:04 +0100

    Added documentation to error_stats.py

---
+++ docs/build/html/_sources/index.rst.txt
@@ -0,0 +1,23 @@
+.. SS-Utilities documentation master file, created by
+   sphinx-quickstart on Mon Sep 10 11:07:07 2018.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to SS-Utilities's documentation!
+========================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+   
+   self
+   modules
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
--- ss_utilities/error_stats.py
+++ ss_utilities/error_stats.py
@@ -8,14 +8,99 @@ Jamie Taylor
 import numpy as np
 
 def r_squared(predictions, actuals):
+    r"""
+    Calculate the coefficient of determination (a.k.a R-Squared) [1]_.
+    
+    Parameters
+    ----------
+    `predictions` : numpy array of floats
+        Predictions being tested.
+    `actuals`: numpy array of floats
+        Actual values corresponding to `predictions`. Must be same size as `predictions`.
+
+    Returns
+    -------
+    float
+        Coefficient of determination.
+
+    Notes
+    -----
+    .. math::
+        \begin{align*}
+        y=Actuals,\quad f&=Predictions,\quad \bar{y}=\frac{1}{n}\sum_{i=1}^n{y_i}\\
+        SS_{tot}&=\sum_i{(y_i-\bar{y})^2}\\
+        SS_{res}&=\sum_i{(y_i-f_i)^2}\\
+        R^2&=1-\frac{SS_{res}}{SS_{tot}}
+        \end{align*}
+
+    References
+    ----------
+    .. [1] https://en.wikipedia.org/wiki/Coefficient_of_determination
+    """
     mean_actual = np.mean(actuals)
     ss_tot = np.sum(np.power(actuals - mean_actual, 2))
     ss_res = np.sum(np.power(actuals - predictions, 2))
     return 1 - ss_res / ss_tot
 
 def pearson_coefficient(predictions, actuals):
+    r"""
+    Calculate the Pearson correlation coefficient [1]_.
+    
+    Parameters
+    ----------
+    `predictions` : numpy array of floats
+        Predictions being tested.
+    `actuals`: numpy array of floats
+        Actual values corresponding to `predictions`. Must be same size as `predictions`.
+
+    Returns
+    -------
+    float
+        Pearson correlation coefficient.
+
+    Notes
+    -----
+    .. math::
+        \begin{align*}
+        \rho_{x,y}=\frac{\mathrm{cov}(x, y)}{\sigma_x\sigma_y}
+        \end{align*}
+
+    References
+    ----------
+    .. [1] https://en.wikipedia.org/wiki/Pearson_correlation_coefficient
+    """
     return np.corrcoef(predictions, actuals)[0, 1]
 
-def wmape(predictions, actuals, norms):
+def wmape(predictions, actuals, norms=None, weights=None):
+    r"""
+    Calculate the weighted Mean Absolute Percent Error (MAPE).
+    
+    Parameters
+    ----------
+    `predictions` : numpy array of floats
+        Predictions being tested.
+    `actuals` : numpy array of floats
+        Actual values corresponding to `predictions`. Must be same size as `predictions`.
+    `norms` : numpy array of floats
+        Normalisation values. Must be same size as `predictions`. Default is to use `actuals`.
+    `weights` : numpy array of floats
+        Weighting values. Must be same size as `predictions`. Default is to use `actuals`.
+
+    Returns
+    -------
+    float
+        wMAPE.
+
+    Notes
+    -----
+    .. math::
+        \begin{gathered}
+        y=Actuals,\quad f=Predictions,\quad n=Normalisations,\quad w=Weights\\
+        \mathit{wMAPE}=
+        \frac{\sum_i{w_i\times\mathrm{abs}\left(\frac{f_i-y_i}{n_i}\right)\times100\%}}{\sum_i{w_i}}
+        \end{gathered}
+    """
+    norms = actuals if norms is None else norms
+    weights = actuals if weights is None else weights
     mapes = np.abs((predictions - actuals) / norms) * 100.
-    return np.sum(actuals * mapes) / np.sum(actuals)
+    return np.sum(weights * mapes) / np.sum(weights)
